7a86e1afab41d74d8e0bfac11e7b34980caab2af
stuffy3 <tannerstufflebeam@gmail.com>
2022-05-03 12:50:41 -0500

    finished const variables

---
+++ variables/constants.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	const c = 18600 //assign a constant by using the const keyword"
+	fmt.Println("The speed of light is", c, "miles per second!")
+}
--- variables/var.go
+++ variables/var.go
@@ -9,12 +9,12 @@ func main() {
 	course := "Getting Started With Go"
 
 	fmt.Println("\nHi", name, "your current course is", course)
-	updateCourse(course)
+	updateCourse(&course)
 	fmt.Println("You're currently Watching", course)
 }
 
-func updateCourse(course string) string {
-	course = "Getting Started with Docker"
-	fmt.Println("Updated course to" course)
-	return course
+func updateCourse(course *string) string {
+	*course = "Getting Started with Docker"
+	fmt.Println("Updated course to", *course)
+	return *course
 }
