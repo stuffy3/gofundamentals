0cb6b0a40dddba5e8130c9ec1dee27283470e80c
stuffy3 <tannerstufflebeam@gmail.com>
2022-05-05 13:15:58 -0500

    added functions

---
+++ functions/func.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	text := "containers on aws wavelength"
+	fmt.Println(titlecase(text))
+}
+
+
+
+// func funcname(params, type) type {
+//  <code>
+// }
